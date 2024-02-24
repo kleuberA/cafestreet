f7a9d1c98c732868d8a248ba4d0eb892b3f89091
Kleuber Silva <kleuber20@hotmail.com>
2024-02-24 09:52:57 -0300

    Add responsiveness in hero section

--- src/components/herosection/hero.tsx
+++ src/components/herosection/hero.tsx
@@ -11,10 +11,10 @@ export default function Hero() {
             <Image src={BgImage} alt={""} className="absolute w-full h-full" />
             <div className="z-10 relative">
                 <NavBar />
-                <div className="w-[80dvw] mx-auto h-[70dvh] flex flex-row items-center">
-                    <div className="flex flex-col gap-3 flex-1">
-                        <h1 className="text-6xl font-bold w-[45dvw] text-[#2F2105]">Enjoy your <span className='text-[#FF902B]'>coffee</span> before your activity</h1>
-                        <span className='text-lg text-[#7E7D7A] w-[24dvw]'>Boost your productivity and build your mood with a glass of coffee in the morning</span>
+                <div className="w-[80dvw] mx-auto h-[70dvh] flex flex-col lg:flex-row items-center pt-10 lg:pt-0">
+                    <div className="flex flex-col gap-3 h-72 lg:h-0 lg:flex-1">
+                        <h1 className="text-2xl lg:text-6xl font-bold lg:w-[45dvw] text-[#2F2105]">Enjoy your <span className='text-[#FF902B]'>coffee</span> before your activity</h1>
+                        <span className='text-base lg:text-lg text-[#7E7D7A] lg:w-[24dvw]'>Boost your productivity and build your mood with a glass of coffee in the morning</span>
                         <div className="flex flex-row gap-10">
                             <button className="rounded-full flex flex-row gap-2 items-center h-12 bg-[#2F2105] hover:bg-opacity-85 transition-all duration-300 p-3 w-36 text-white">
                                 Order Now
